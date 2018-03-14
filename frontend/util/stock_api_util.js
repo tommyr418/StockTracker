c873ba1aff21f7bcaa29cf385e54948f7d1c60dd
Tommy Ren <tommyr418@gmail.com>
2018-03-13 20:42:42 -0400

    add api util for fetching data

---
+++ frontend/util/stock_api_util.js
@@ -0,0 +1,6 @@
+export const requestBatchQuotes = () => (
+  $.ajax({
+    method: "GET",
+    url: "https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=MSFT,FB,AAPL,GOOGL,AMZN&apikey=0EVY2TNG7FN9V6N4",
+  })
+);
