91f6256646eec8cc64cc71d0252e527cd7ff41ea
Tommy Ren <tommyr418@gmail.com>
2018-03-13 21:03:42 -0400

    combine reducer into root rducer, add store

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import EntitiesReducer from './entities_reducer';
+
+const RootReducer = combineReducers({
+  entities: EntitiesReducer,
+});
+
+export default RootReducer;
