4f0b6d15189ef5ac2be680ac8413126db75a6112
Tommy Ren <tommyr418@gmail.com>
2018-03-15 14:54:52 -0400

    fix stlyling, begin company page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,7 +5,7 @@ import LandingPageContainer from './pages/landing_page_container';
 import MainSideNav from './modules/main_side_nav';
 
 const App = () => (
-  <div>
+  <div id="app">
     <Route path='/' component={ MainSideNav }/>
     <Route path='/quick-quotes' component={ LandingPageContainer }/>
     <Route exact path='/' component={ LandingPageContainer }/>
